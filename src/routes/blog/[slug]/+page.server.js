7aab9189469d4482589036760f0fae996bfb9260
SG7504 <gamewizard750@gmail.com>
2024-07-03 10:59:36 +0530

    commit

---
+++ src/routes/blog/[slug]/+page.server.js
@@ -0,0 +1,7 @@
+export function load (params) {
+
+    const data = ${params}
+    return {
+        content: `hello {$params.slug}`
+    } 
+}
---
+++ src/routes/blog/list/+page.svelte
@@ -0,0 +1,4 @@
+<script>
+    import Table from "$lib/Table.svelte"
+</script>
+<h1>list of blogs </h1>
